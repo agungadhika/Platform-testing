410055ab6d26f2899f7fa838a06a834c411bbc65
agungadhika <agungadhika@gmail.com>
2023-04-09 21:58:24 +0800

    sqlinjection, tambah 1 lagi. next xss

---
+++ SQLInjection/chall1/index.php
@@ -0,0 +1,26 @@
+<?php
+// Menghubungkan ke database
+$host = "localhost";
+$user = "root";
+$password = "ikantongkol";
+$database = "tugasakhir";
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+// Menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Menyeleksi data user dari database
+$query = "SELECT * FROM mahasiswa WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($koneksi, $query);
+
+// Menghitung jumlah data yang ditemukan
+$count = mysqli_num_rows($result);
+
+// Mengecek apakah data ditemukan
+if ($count == 1) {
+	echo "Login berhasil!";
+} else {
+	echo "Login gagal!";
+}
+?>
