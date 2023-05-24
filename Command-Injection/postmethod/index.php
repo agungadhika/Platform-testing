518b55fbf7902f9c8ad63237544e9414a49ab3da
agungadhika <agungadhika@gmail.com>
2023-05-24 15:41:34 +0800

    fix command-injection. soon testing in dejection

--- Command-Injection/postmethod/index.php
+++ Command-Injection/postmethod/index.php
@@ -1,13 +1,35 @@
-<?php
-  // Mengecek apakah tombol submit ditekan
-  if(isset($_POST['host'])) {
-    // Mengambil nilai input dari form
-    $host = $_POST['host'];
-    
-    // Menjalankan perintah ping pada host menggunakan fungsi shell_exec()
-    $output = shell_exec('ping -c 3 '.$host);
-    
-    // Menampilkan hasil ping ke dalam tag pre
-    echo '<pre>'.$output.'</pre>';
-  }
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Form Ping Jaringan</title>
+  </head>
+  <body>
+    <h1>Form Ping Jaringan</h1>
+    <?php
+      // Mengecek apakah tombol submit ditekan
+      if(isset($_POST['host'])) {
+        // Mengambil nilai input dari form
+        $host = $_POST['host'];
+
+        // Menjalankan perintah ping pada host menggunakan fungsi shell_exec()
+        $output = shell_exec('ping -c 3 '.$host);
+
+        // Menghitung panjang output
+        $panjang_output = strlen($output);
+
+        // Mengatur header Content-Length
+        header('Content-Length: ' . $panjang_output);
+
+        // Menampilkan hasil ping ke dalam tag pre
+        echo '<pre>'.$output.'</pre>';
+      } else {
+        // Menampilkan form jika tombol submit belum ditekan
+    ?>
+    <form action="" method="post">
+      <label for="host">Host:</label>
+      <input type="text" id="host" name="host"><br><br>
+      <input type="submit" value="Ping">
+    </form>
+    <?php } ?>
+  </body>
+</html>
